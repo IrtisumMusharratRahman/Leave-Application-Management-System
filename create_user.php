6ec67e23bb4e8ff91e345734eb8acd605a551c50
monirulislam97 <86794031+monirulislam97@users.noreply.github.com>
2021-07-07 21:52:04 +0800

    dom

--- create_user.php
+++ create_user.php
@@ -3,13 +3,11 @@
 require_once ("config.php");
 
 
-$sql = "CREATE TABLE user
-        (
+$sql = "CREATE TABLE user(
  		username varchar(100),
     id varchar(15),
  		password varchar(13),
-		level int(3)
-        )";
+		level int(3))";
 
 if (mysqli_query($conn, $sql)) {
   echo "<h3>Table user created successfully</h3>";
