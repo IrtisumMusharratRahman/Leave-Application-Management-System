6a4df827e7305a1918ffc643bbd8161525456efd
Shafayet92 <85796731+Shafayet92@users.noreply.github.com>
2021-07-08 19:48:54 +0800

    d

--- report.php
+++ report.php
@@ -30,7 +30,7 @@ header("Location: login.php");
 		</tr>
 
 		<?php
-		$rows = mysqli_fetch_assoc($result)
+		$rows = mysqli_fetch_assoc($result);
 		?>
 
 		<tr>
@@ -39,7 +39,7 @@ header("Location: login.php");
 			<td><?php echo $rows['days']; ?></td>
       <td><?php echo $rows['reason']; ?></td>
 		</tr>
-		
+		}
 
 		<?php 
 
