36ddf4d63dc39ce475b7a16b380d5fc5143ae10a
monirulislam97 <86794031+monirulislam97@users.noreply.github.com>
2021-07-09 21:05:05 +0800

    manager

--- manager_main.php
+++ manager_main.php
@@ -13,10 +13,10 @@
   <div id="mySidenav" class="sidenav">
   <p class="level">Manager</p>
   <a href="#" class="icon-a"><i class="fa fa-dashboard icons"></i> &nbsp;&nbsp;Dashboard</a>
+  <a href="update_info_form.php"class="icon-a"><i class="fa fa-user icons"></i> &nbsp;&nbsp;Edit Profile</a>
   <a href="forms_report.php" class="icon-a"><i class="fa fa-edit"></i> &nbsp;&nbsp; View Aspplications</a>
   <a href="report.php"class="icon-a"><i class="fa fa-file"></i> &nbsp;&nbsp; View Report</a>
   <a href="search_form.php"class="icon-a"><i class="fa fa-search" ></i> &nbsp;&nbsp;Search A Staff</a>
-  <a href="#"class="icon-a"><i class="fa fa-user icons"></i> &nbsp;&nbsp;Accounts</a>
   <br>
   <br>
   <a href="logout.php"class="icon-a"><i class="fa fa-sign-out"></i> &nbsp;&nbsp;Logout</a>
