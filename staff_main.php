6b125ab93b7ca44e1664c19d3a59b3019e119e94
Nayeeem <nayeemzz16@gmail.com>
2021-07-09 23:02:27 +0800

    staff main

--- staff_main.php
+++ staff_main.php
@@ -28,8 +28,8 @@
   <!--sidebar start-->
   <div class="sidebar">
     <center>
-      <img class="profile_image" alt="">
-      <h4>Chad</h4>
+      <img src= "./css/images/user.png" class="profile_image" alt="">
+      <h4><?php echo $_SESSION["USER"] ?> </h4>
     </center>
     <a href="update_info_form.php"><i class="fas fa-user"></i><span>Edit Profile</span></a>
     <a href="leave_form.php"><i class="fa fa-th"></i><span>Leave Form</span></a>
