a0bd7adb46cb770f09b284da2575c497ebdd3604
monirulislam97 <86794031+monirulislam97@users.noreply.github.com>
2021-07-09 21:59:03 +0800

    report

--- report.php
+++ report.php
@@ -9,13 +9,14 @@ if ($_SESSION["Login"] != "YES") //if the user is not logged in or has been logg
 <html>
 
 <head>
-	<title>Viewing Student Data</title>
+	<title>Leave Applications Reoprt</title>
+	<link rel="stylesheet" href="./css/table_report.css" type="text/css"/>
 
 	<head>
 
 	<body>
 
-		<h1>View Student Details </h1>
+		<h1>Leave Applications Reoprt </h1>
 
 		<?php
 		require("config.php");
@@ -28,19 +29,18 @@ if ($_SESSION["Login"] != "YES") //if the user is not logged in or has been logg
 			<table width="600" border="1" cellspacing="0" cellpadding="3">
 
 				<tr>
-					<td align="center"><strong>Name</strong></td>
-					<td align="center"><strong>ID</strong></td>
-					<td align="center"><strong>Start Date</strong></td>
-					<td align="center"><strong>End Date</strong></td>
-					<td align="center"><strong>Number of Days</strong></td>
-					<td align="center"><strong>Reason</strong></td>
-					<td align="center"><strong>Status</strong></td>
+					<th align="center"><strong>Name</strong></th>
+					<th  align="center"><strong>ID</strong></th>
+					<th  align="center"><strong>Start Date</strong></th>
+					<th  align="center"><strong>End Date</strong></th>
+					<th  align="center"><strong>Number of Days</strong></th>
+					<th  align="center"><strong>Reason</strong></th>
+					<th  align="center"><strong>Status</strong></th>
 				</tr>
 
 				<?php
 				while ($rows = mysqli_fetch_assoc($result)) {
 				?>
-
 					<tr>
 						<td><?php echo $rows['name']; ?></td>
 						<td><?php echo $rows['id']; ?></td>
@@ -52,8 +52,6 @@ if ($_SESSION["Login"] != "YES") //if the user is not logged in or has been logg
 					</tr>
 
 				<?php }
-
-
 				mysqli_close($conn);
 				?>
 
@@ -64,6 +62,9 @@ if ($_SESSION["Login"] != "YES") //if the user is not logged in or has been logg
 
 
 		?>
+
+		<br>
+		<br>
 		<button onclick="window.location.href='check_login.php';">Previous Page</button>
 	</body>
 
